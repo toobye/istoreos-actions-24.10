ed3f2790123d86030261d4628126944673f072af
Jaykwok2999 <61024435@qq.com>
2025-02-08 18:54:52 +0800

    Update diy-immortalwrt.sh

--- diy/diy-immortalwrt.sh
+++ diy/diy-immortalwrt.sh
@@ -6,6 +6,10 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 修改名称
 sed -i 's/ImmortalWrt/iStoreOS/' package/base-files/files/bin/config_generate
 
+##New WiFi
+sed -i "s/ImmortalWrt-2.4G/OpenWrt-2.4G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ImmortalWrt-5G/OpenWrt-5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
 # profile
 sed -i 's#\\u@\\h:\\w\\\$#\\[\\e[32;1m\\][\\u@\\h\\[\\e[0m\\] \\[\\033[01;34m\\]\\W\\[\\033[00m\\]\\[\\e[32;1m\\]]\\[\\e[0m\\]\\\$#g' package/base-files/files/etc/profile
 sed -ri 's/(export PATH=")[^"]*/\1%PATH%:\/opt\/bin:\/opt\/sbin:\/opt\/usr\/bin:\/opt\/usr\/sbin/' package/base-files/files/etc/profile
