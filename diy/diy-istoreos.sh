bd5d15667cf66460edf9badbe615176a7008fc34
Jaykwok2999 <61024435@qq.com>
2025-04-26 16:21:38 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -128,7 +128,7 @@ cp -af feeds/istoreos_ipk/patch/diy/banner package/base-files/files/etc/
 
 # tailscale
 # rm -rf feeds/packages/net/tailscale
-rm -rf feeds/istoreos_ipk/tailscale/tailscale
+# rm -rf feeds/istoreos_ipk/tailscale/tailscale
 # cp -af feeds/istoreos_ipk/tailscale/tailscale  feeds/packages/net/
 sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
 
