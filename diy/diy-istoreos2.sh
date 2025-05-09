c5e8d4183d5750cfc3ea788b151e5b63e11cfc13
Jaykwok2999 <61024435@qq.com>
2025-05-09 11:23:01 +0800

    Update diy-istoreos2.sh

--- diy/diy-istoreos2.sh
+++ diy/diy-istoreos2.sh
@@ -12,7 +12,7 @@ sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/shar
 # 修改默认密码
 sed -i 's/root:::0:99999:7:::/root:$1$5mjCdAB1$Uk1sNbwoqfHxUmzRIeuZK1:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 rm -rf include/version.mk
-cp -af feeds/istoreos_ipk/patch/istoreos-24.10/version.mk include
+cp -af feeds/istoreos_ipk/patch/istoreos-24.10/version.mk include/
 
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
