d873d08f967c1a0fb13d2a1fcdd51f6839101726
Jaykwok2999 <61024435@qq.com>
2025-05-26 01:02:51 +0800

    Update diy-lede.sh

--- diy/diy-lede.sh
+++ diy/diy-lede.sh
@@ -216,7 +216,7 @@ sed -i 's/3.openwrt.pool.ntp.org/time2.cloud.tencent.com/g' package/base-files/f
 
 # 更改 banner
 rm -rf package/base-files/files/etc/banner
-cp -af feeds/istoreos_ipk/patch/diy/OpenWrt/banner package/base-files/files/etc/
+cp -af feeds/istoreos_ipk/patch/diy/OpenWrt/1/banner package/base-files/files/etc/
 
 # 修改登录字符
 rm -rf feeds/luci/modules/luci-base/po/zh_Hans/base.po
