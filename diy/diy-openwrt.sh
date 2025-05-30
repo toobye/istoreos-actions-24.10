76b4ff066d274ab9562fed787b778b940510202b
Jaykwok2999 <61024435@qq.com>
2025-05-30 19:53:54 +0800

    Update diy-openwrt.sh

--- diy/diy-openwrt.sh
+++ diy/diy-openwrt.sh
@@ -161,32 +161,8 @@ git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/l
 git clone https://github.com/Jaykwok2999/luci-app-passwall.git package/passwall -b main
 rm -rf feeds/istoreos_ipk/patch/wall-luci/luci-app-passwall
 
-# Alist
-git clone https://git.kejizero.online/zhao/luci-app-alist package/alist
-
-# Mosdns
-git clone https://git.kejizero.online/zhao/luci-app-mosdns.git -b v5 package/mosdns
-git clone https://git.kejizero.online/zhao/v2ray-geodata.git package/v2ray-geodata
-
 # 锐捷认证
 # git clone https://github.com/sbwml/luci-app-mentohust package/mentohust
-
-# Adguardhome
-# git_sparse_clone master https://github.com/kenzok8/openwrt-packages adguardhome luci-app-adguardhome
-
-# default-settings
-# git clone --depth=1 -b dev https://github.com/Jaykwok2999/default-settings package/default-settings
-
-# UPnP
-rm -rf feeds/{packages/net/miniupnpd,luci/applications/luci-app-upnp}
-git clone https://git.kejizero.online/zhao/miniupnpd feeds/packages/net/miniupnpd -b v2.3.7
-git clone https://git.kejizero.online/zhao/luci-app-upnp feeds/luci/applications/luci-app-upnp -b master
-
-# Lucky
-# git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-
-# luci-app-webdav
-# git clone https://git.kejizero.online/zhao/luci-app-webdav package/new/luci-app-webdav
 
 # unzip
 rm -rf feeds/packages/utils/unzip
