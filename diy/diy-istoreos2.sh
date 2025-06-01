82effdea1bd6aaec5ba5c2b569abbed42bc5a58c
Jaykwok2999 <61024435@qq.com>
2025-06-01 22:54:21 +0800

    Update diy-istoreos2.sh

--- diy/diy-istoreos2.sh
+++ diy/diy-istoreos2.sh
@@ -17,6 +17,9 @@ sed -i 's/root:::0:99999:7:::/root:$1$5mjCdAB1$Uk1sNbwoqfHxUmzRIeuZK1:0:0:99999:
 rm -rf include/version.mk
 cp -af feeds/istoreos_ipk/patch/istoreos-24.10/version.mk include
 
+# iStoreOS-settings
+git clone --depth=1 -b main https://github.com/Jaykwok2999/istoreos-settings package/default-settings
+
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
