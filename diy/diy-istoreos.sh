22ebfa312ea725a399db0091547ea6be293c2291
Jaykwok2999 <61024435@qq.com>
2025-06-01 22:33:37 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -132,7 +132,7 @@ pushd feeds/luci
 popd
 
 pushd
-   curl -sSL https://raw.githubusercontent.com/Jaykwok2999/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
+   curl -sSL https://raw.githubusercontent.com/Jaykwok2999/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 popd
 
 # NTP
